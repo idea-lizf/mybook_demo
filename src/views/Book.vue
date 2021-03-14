3f9c1fe10d92e2263ccc9197d586c75432ff433d
idea-ali <1836449942@qq.com>
2021-03-14 21:34:16 +0800

    先构建文件结构

---
+++ src/views/Book.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <table>
+      <tr v-for = "(item, index) in stu" :key="index">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.message}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  data() {
+    return {
+      stu: [
+        {
+          id: 'a001',
+          name: 'xiaoming',
+          message: 'admin',
+        },
+        {
+          id: 'a002',
+          name: 'xiaobai',
+          message: 'admal',
+        }
+      ],
+
+    }
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
